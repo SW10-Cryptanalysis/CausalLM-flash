864cebd776777dbaa78d585e05561dc57b984d10
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2026-03-23 11:45:20 +0100

    fix: added PadCollator to class init

--- src/classes/__init__.py
+++ src/classes/__init__.py
@@ -2,5 +2,6 @@
 
 from .config import Config
 from .dataset import CipherPlainData
+from .pad_collator import PadCollator
 
-__all__ = ["Config", "CipherPlainData"]
+__all__ = ["Config", "CipherPlainData", "PadCollator"]
